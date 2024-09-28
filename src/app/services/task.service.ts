e20cf3226085f62915f254a544a2beef4ec93f14
Carlos <41967190+koh81@users.noreply.github.com>
2024-09-28 16:19:06 -0500

    Service created
    CRUD functions created

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,9 +1,44 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { Task } from '../interfaces/task.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
+  private tasks: Task[] = [];
+  private tasksSubject = new BehaviorSubject<Task[]>([]);
 
-  constructor() { }
+  constructor() {}
+
+  getTasks(): Observable<Task[]> {
+    return this.tasksSubject.asObservable();
+  }
+
+  addTask(task: Task): void {
+    task.id = this.tasks.length + 1;
+    this.tasks.push(task);
+    this.tasksSubject.next([...this.tasks]);
+  }
+
+  updateTask(task: Task): void {
+    const index = this.tasks.findIndex(t => t.id === task.id);
+    if (index !== -1) {
+      this.tasks[index] = task;
+      this.tasksSubject.next([...this.tasks]);
+    }
+  }
+
+  deleteTask(taskId: number): void {
+    this.tasks = this.tasks.filter(t => t.id !== taskId);
+    this.tasksSubject.next([...this.tasks]);
+  }
+
+  toggleTaskCompletion(taskId: number): void {
+    const task = this.tasks.find(t => t.id === taskId);
+    if (task) {
+      task.completed = !task.completed;
+      this.tasksSubject.next([...this.tasks]);
+    }
+  }
 }
