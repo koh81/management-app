1e4ccc03f2cbf1d64c513f7eb28dbea1a7b7524b
Carlos <41967190+koh81@users.noreply.github.com>
2024-09-28 15:30:26 -0500

    Base structure

---
+++ src/app/components/person-form/person-form.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-person-form',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './person-form.component.html',
+  styleUrls: ['./person-form.component.css']
+})
+export class PersonFormComponent {
+
+}
